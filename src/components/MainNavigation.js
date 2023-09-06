be7acb31e8b2265b8f32152f59159e6806a8b611
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-09-06 16:01:52 +0300

    latest for max-react-router

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,22 @@
+import { Link as NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Products</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
